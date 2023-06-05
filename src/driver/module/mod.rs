91e9cc10c1d736e7f00a18f3b8368826b2d15159
Brian <brian030128@gmail.com>
2023-06-05 13:16:12 +0800

    hide my password ....

--- src/driver/module/mod.rs
+++ src/driver/module/mod.rs
@@ -9,7 +9,7 @@ pub struct Modules{
 impl Modules{
 
     pub async fn new()->Modules {
-        let driver = Arc::new(DatabaseDriver::new(DatabaseConfig::new("mongodb+srv://brian920128:c177267w@cluster0.hek6yds.mongodb.net/?retryWrites=true&w=majority".to_owned(), "life-butler"))
+        let driver = Arc::new(DatabaseDriver::new(DatabaseConfig::new("mongodb+srv://brian920128:password@cluster0.hek6yds.mongodb.net/?retryWrites=true&w=majority".to_owned(), "life-butler"))
             .await
             .unwrap());
         let repositories = RepositoriesModule::new(driver);
