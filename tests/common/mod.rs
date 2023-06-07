2104a618a778be004101e01e4b650dc8df7eccd1
Brian <brian030128@gmail.com>
2023-06-07 18:12:27 +0800

    renew mongodb password

--- tests/common/mod.rs
+++ tests/common/mod.rs
@@ -10,7 +10,7 @@ static MODULES: OnceCell<Modules> = OnceCell::const_new();
 
 pub async fn setup() -> &'static Modules{
     return MODULES.get_or_init( || async {
-        let password = String::from("c177267w");
+        let password = String::from("M9VWh2oRhEbUNjBx");
         Modules::new(password.as_str()).await
     }).await;
 }
